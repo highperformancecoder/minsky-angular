58cf8bf02b944a5289de21899cfda39a21f7c755
RaviMrk <raviparmar12316@gmail.com>
2020-08-17 13:34:22 +0530

    components created

---
+++ src/app/components/wiring/wiring.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wiring',
+  templateUrl: './wiring.component.html',
+  styleUrls: ['./wiring.component.css']
+})
+export class WiringComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
