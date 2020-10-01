2fa9501d810ce404fbfcaa16dd7e425b695ea7b3
anshu-coditas <anshu.srivastava@coditas.com>
2020-10-01 15:40:05 +0530

    changes for css

--- src/app/test/components/godley-table.component.spec.ts
+++ src/app/test/components/godley-table.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { GodleyTableComponent } from './godley-table.component'
+import { GodleyTableComponent } from '../../components/menu/insert/godley-table/godley-table.component'
 
 describe('GodleyTableComponent', () => {
 	let component: GodleyTableComponent
