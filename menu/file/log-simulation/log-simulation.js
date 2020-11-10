7a1732efad8f76a27e10c915c7d961c05e0adc23
Ravi <ravi.rajpurohit385@gmail.com>
2020-11-10 12:05:15 +0530

    deleted unwanted files

--- menu/file/log-simulation/log-simulation.js
+++
@@ -1,5 +0,0 @@
-const electron = require('electron')
-const { ipcRenderer } = electron
-document.querySelector('.ok-btn').addEventListener('click', () => {
-	ipcRenderer.send('global-menu-popup:cancel')
-})
--- menu/godley-table/godley-table.js
+++
@@ -1,9 +0,0 @@
-const electron = require('electron')
-const { ipcRenderer } = electron
-document.getElementById('godleyTableOk').addEventListener('click', () => {
-	console.log('button clicked')
-	ipcRenderer.send('godley-table:ok', '')
-})
-document.querySelector('.cancel-btn').addEventListener('click', () => {
-	ipcRenderer.send('global-menu-popup:cancel')
-})
