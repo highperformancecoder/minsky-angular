4d46c3047618d7d96441a2300294b46eecddaf76
Janak Porwal <janak@digiperfect.com>
2021-05-04 15:49:40 +0530

    html heirarchy for canvas changed

--- libs/shared/src/lib/WindowUtilitiesGlobal.ts
+++ libs/shared/src/lib/WindowUtilitiesGlobal.ts
@@ -5,40 +5,50 @@ interface Offset {
 
 abstract class WindowUtilitiesGlobal {
   private static minskyCanvasElement: HTMLElement = null;
+  private static minskyCanvasContainer : HTMLElement = null;
   private static leftOffset = 0;
   private static topOffset = 0;
+  private static drawableWidth = 0;
+  private static drawableHeight = 0;
+
 
   private static initializeIfNeeded() {
     if (!this.minskyCanvasElement) {
-      this.minskyCanvasElement = document.getElementById('minsky-canvas-container');
+      const elements = document.getElementsByClassName('minsky-canvas-container');
+      if(elements) {
+        this.minskyCanvasContainer = elements[0] as HTMLElement;
+
+        this.minskyCanvasElement = document.getElementById('main-minsky-canvas');
+        
+        this.drawableWidth = this.minskyCanvasContainer.clientWidth;
+        this.drawableHeight = this.minskyCanvasContainer.clientHeight;
+
+        // TODO:: Review ---> Canvas dimenstions 10X of container
+
+        this.minskyCanvasElement.style.width = 10*this.drawableWidth + "px";
+        this.minskyCanvasElement.style.height = 10*this.drawableHeight + "px";
+
+        const clientRect = this.minskyCanvasContainer.getBoundingClientRect();
+        this.leftOffset = Math.ceil(clientRect.left);
+        this.topOffset = Math.ceil(clientRect.top);
+      }
     }
   }
 
   public static getMinskyCanvasOffset(): Offset {
     this.initializeIfNeeded();
-    if (this.minskyCanvasElement) {
-      const clientRect = this.minskyCanvasElement.getBoundingClientRect();
-      this.leftOffset = clientRect.left;
-      this.topOffset = clientRect.top;
-      // // if (this.leftOffset === null) {
-      // // this.minskyCanvasElement.offsetLeft;
-      // this.leftOffset =
-      //  (window.pageXOffset || this.minskyCanvasElement.scrollLeft) - 
-      //   (this.minskyCanvasElement.clientLeft || 0);
-      // // }
-
-      // // if (this.topOffset === null) {
-      // // this.minskyCanvasElement.offsetTop
-      // this.topOffset =
-      //   (window.pageYOffset || this.minskyCanvasElement.scrollTop) -
-      //   (this.minskyCanvasElement.clientTop || 0);
-      // // }
-    }
-    console.log(this.topOffset, this.leftOffset);
     return {
       left: this.leftOffset,
       top: this.topOffset,
     };
+  }
+  
+  public static getDrawableArea() {
+    this.initializeIfNeeded();
+    return {
+      width : this.drawableWidth,
+      height : this.drawableHeight
+    }
   }
 
   public static getMinskyCanvasElement(): HTMLElement {
