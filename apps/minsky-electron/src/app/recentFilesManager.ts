543e15cb189bb94b6f6a53b2a81401a0f99d876b
Ravi <ravi.rajpurohit385@gmail.com>
2021-07-02 16:17:34 +0530

    fixed: issues with recent menu

--- apps/minsky-electron/src/app/recentFilesManager.ts
+++ apps/minsky-electron/src/app/recentFilesManager.ts
@@ -8,51 +8,54 @@ export class RecentFilesManager {
 
     const exists = Boolean(recentFiles.find((f) => f === filepath));
     if (!exists) {
-      const newRecentFiles = this.getRecentFilesThatMatchesRecentFilesToDisplay(
-        { recentFiles }
-      );
+      recentFiles.push(filepath);
 
-      newRecentFiles.push(filepath);
-      StoreManager.store.set('recentFiles', newRecentFiles);
+      StoreManager.store.set('recentFiles', recentFiles);
     }
   }
 
   static initRecentFiles() {
     const recentFiles = StoreManager.store.get('recentFiles');
 
+    const newRecentFiles = this.getRecentFilesThatMatchesRecentFilesToDisplay(
+      recentFiles
+    );
+
+    if (recentFiles.length !== newRecentFiles.length) {
+      StoreManager.store.set('recentFiles', newRecentFiles);
+    }
+
+    this.updateRecentFilesMenu(newRecentFiles);
+  }
+
+  private static updateRecentFilesMenu(newRecentFiles: string[]) {
     const openRecentMenu = Menu.getApplicationMenu().getMenuItemById(
       'openRecent'
     );
 
-    const newRecentFiles = this.getRecentFilesThatMatchesRecentFilesToDisplay({
-      recentFiles,
+    openRecentMenu.submenu.items.forEach((f) => {
+      if (f.id !== 'clearRecent') {
+        f.visible = false;
+      }
     });
 
     newRecentFiles.forEach((filePath) => {
-      const menuItem = openRecentMenu.submenu.items.find(
-        (f) => f.label === filePath
+      const position = 0;
+      openRecentMenu.submenu.insert(
+        position,
+        new MenuItem({
+          label: filePath,
+          click: async () => {
+            await CommandsManager.openNamedFile(filePath);
+          },
+        })
       );
-
-      if (menuItem && !menuItem.enabled) {
-        menuItem.enabled = true;
-      } else {
-        const position = 0;
-        openRecentMenu.submenu.insert(
-          position,
-          new MenuItem({
-            label: filePath,
-            click: async () => {
-              await CommandsManager.openNamedFile(filePath);
-            },
-          })
-        );
-      }
     });
   }
 
-  private static getRecentFilesThatMatchesRecentFilesToDisplay({
-    recentFiles,
-  }) {
+  private static getRecentFilesThatMatchesRecentFilesToDisplay(
+    recentFiles: string[]
+  ) {
     const numberOfRecentFiles = recentFiles.length;
 
     const numberOfRecentFilesToDisplay = StoreManager.store.get('preferences')
@@ -65,25 +68,22 @@ export class RecentFilesManager {
       const numberOfItemsToBeRemoved =
         numberOfRecentFiles - numberOfRecentFilesToDisplay;
 
-      if (numberOfItemsToBeRemoved === 0) {
-        const newRecentFiles = recentFiles.shift();
-        return newRecentFiles;
-      }
-
       if (numberOfItemsToBeRemoved > 0) {
         const newRecentFiles = recentFiles.slice(numberOfItemsToBeRemoved);
+
         return newRecentFiles;
       }
     }
 
     return recentFiles;
   }
+
   static updateNumberOfRecentFilesToDisplay() {
     const recentFiles = StoreManager.store.get('recentFiles');
 
-    const newRecentFiles = this.getRecentFilesThatMatchesRecentFilesToDisplay({
-      recentFiles,
-    });
+    const newRecentFiles = this.getRecentFilesThatMatchesRecentFilesToDisplay(
+      recentFiles
+    );
 
     StoreManager.store.set('recentFiles', newRecentFiles);
   }
